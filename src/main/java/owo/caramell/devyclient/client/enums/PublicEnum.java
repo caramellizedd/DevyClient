edaf88d092f6639c83f71a0e73e6f06523a362cf
caramellizedd <80157027+caramellizedd@users.noreply.github.com>
2025-07-05 18:00:24 +0700

    Cleanup status bar code.

--- src/main/java/owo/caramell/devyclient/client/enums/PublicEnum.java
+++ src/main/java/owo/caramell/devyclient/client/enums/PublicEnum.java
@@ -7,47 +7,17 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 
 public class PublicEnum {
+    // Credits goes to Mojang.
+    // Modified by CRML Studios.
+    // Used for creating the "simplified" status bars
     @Environment(value= EnvType.CLIENT)
     public enum HeartType {
-        CONTAINER(Identifier.ofVanilla("hud/heart/container"), Identifier.ofVanilla("hud/heart/container_blinking"), Identifier.ofVanilla("hud/heart/container"), Identifier.ofVanilla("hud/heart/container_blinking"), Identifier.ofVanilla("hud/heart/container_hardcore"), Identifier.ofVanilla("hud/heart/container_hardcore_blinking"), Identifier.ofVanilla("hud/heart/container_hardcore"), Identifier.ofVanilla("hud/heart/container_hardcore_blinking")),
-        NORMAL(Identifier.ofVanilla("hud/heart/full"), Identifier.ofVanilla("hud/heart/full_blinking"), Identifier.ofVanilla("hud/heart/half"), Identifier.ofVanilla("hud/heart/half_blinking"), Identifier.ofVanilla("hud/heart/hardcore_full"), Identifier.ofVanilla("hud/heart/hardcore_full_blinking"), Identifier.ofVanilla("hud/heart/hardcore_half"), Identifier.ofVanilla("hud/heart/hardcore_half_blinking")),
-        POISONED(Identifier.ofVanilla("hud/heart/poisoned_full"), Identifier.ofVanilla("hud/heart/poisoned_full_blinking"), Identifier.ofVanilla("hud/heart/poisoned_half"), Identifier.ofVanilla("hud/heart/poisoned_half_blinking"), Identifier.ofVanilla("hud/heart/poisoned_hardcore_full"), Identifier.ofVanilla("hud/heart/poisoned_hardcore_full_blinking"), Identifier.ofVanilla("hud/heart/poisoned_hardcore_half"), Identifier.ofVanilla("hud/heart/poisoned_hardcore_half_blinking")),
-        WITHERED(Identifier.ofVanilla("hud/heart/withered_full"), Identifier.ofVanilla("hud/heart/withered_full_blinking"), Identifier.ofVanilla("hud/heart/withered_half"), Identifier.ofVanilla("hud/heart/withered_half_blinking"), Identifier.ofVanilla("hud/heart/withered_hardcore_full"), Identifier.ofVanilla("hud/heart/withered_hardcore_full_blinking"), Identifier.ofVanilla("hud/heart/withered_hardcore_half"), Identifier.ofVanilla("hud/heart/withered_hardcore_half_blinking")),
-        ABSORBING(Identifier.ofVanilla("hud/heart/absorbing_full"), Identifier.ofVanilla("hud/heart/absorbing_full_blinking"), Identifier.ofVanilla("hud/heart/absorbing_half"), Identifier.ofVanilla("hud/heart/absorbing_half_blinking"), Identifier.ofVanilla("hud/heart/absorbing_hardcore_full"), Identifier.ofVanilla("hud/heart/absorbing_hardcore_full_blinking"), Identifier.ofVanilla("hud/heart/absorbing_hardcore_half"), Identifier.ofVanilla("hud/heart/absorbing_hardcore_half_blinking")),
-        FROZEN(Identifier.ofVanilla("hud/heart/frozen_full"), Identifier.ofVanilla("hud/heart/frozen_full_blinking"), Identifier.ofVanilla("hud/heart/frozen_half"), Identifier.ofVanilla("hud/heart/frozen_half_blinking"), Identifier.ofVanilla("hud/heart/frozen_hardcore_full"), Identifier.ofVanilla("hud/heart/frozen_hardcore_full_blinking"), Identifier.ofVanilla("hud/heart/frozen_hardcore_half"), Identifier.ofVanilla("hud/heart/frozen_hardcore_half_blinking"));
-
-        private final Identifier fullTexture;
-        private final Identifier fullBlinkingTexture;
-        private final Identifier halfTexture;
-        private final Identifier halfBlinkingTexture;
-        private final Identifier hardcoreFullTexture;
-        private final Identifier hardcoreFullBlinkingTexture;
-        private final Identifier hardcoreHalfTexture;
-        private final Identifier hardcoreHalfBlinkingTexture;
-
-        private HeartType(Identifier fullTexture, Identifier fullBlinkingTexture, Identifier halfTexture, Identifier halfBlinkingTexture, Identifier hardcoreFullTexture, Identifier hardcoreFullBlinkingTexture, Identifier hardcoreHalfTexture, Identifier hardcoreHalfBlinkingTexture) {
-            this.fullTexture = fullTexture;
-            this.fullBlinkingTexture = fullBlinkingTexture;
-            this.halfTexture = halfTexture;
-            this.halfBlinkingTexture = halfBlinkingTexture;
-            this.hardcoreFullTexture = hardcoreFullTexture;
-            this.hardcoreFullBlinkingTexture = hardcoreFullBlinkingTexture;
-            this.hardcoreHalfTexture = hardcoreHalfTexture;
-            this.hardcoreHalfBlinkingTexture = hardcoreHalfBlinkingTexture;
-        }
-
-        public Identifier getTexture(boolean hardcore, boolean half, boolean blinking) {
-            if (!hardcore) {
-                if (half) {
-                    return blinking ? this.halfBlinkingTexture : this.halfTexture;
-                }
-                return blinking ? this.fullBlinkingTexture : this.fullTexture;
-            }
-            if (half) {
-                return blinking ? this.hardcoreHalfBlinkingTexture : this.hardcoreHalfTexture;
-            }
-            return blinking ? this.hardcoreFullBlinkingTexture : this.hardcoreFullTexture;
-        }
+        CONTAINER(),
+        NORMAL(),
+        POISONED(),
+        WITHERED(),
+        ABSORBING(),
+        FROZEN();
 
         public static PublicEnum.HeartType fromPlayerState(PlayerEntity player) {
             PublicEnum.HeartType heartType = player.hasStatusEffect(StatusEffects.POISON) ? POISONED : (player.hasStatusEffect(StatusEffects.WITHER) ? WITHERED : (player.isFrozen() ? FROZEN : NORMAL));
